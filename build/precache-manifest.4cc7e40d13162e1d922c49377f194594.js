0efd1d7afa7b98a07afdc45697efc46ef93270e7
yangdepp <965093975@qq.com>
2018-10-21 17:55:43 +0800

    第一次build

---
+++ build/precache-manifest.4cc7e40d13162e1d922c49377f194594.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "38b497436389024c020a",
+    "url": "/static/js/main.38b49743.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "38b497436389024c020a",
+    "url": "/static/css/main.b8154130.chunk.css"
+  },
+  {
+    "revision": "2b002d48d13c7823bd407bc470cb7c77",
+    "url": "/index.html"
+  }
+];
