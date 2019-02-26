bb47c3a8c2b0f5b954b958c161bc2c075934cbde
yangdepp <965093975@qq.com>
2019-02-26 23:56:27 +0800

    赛跑done

--- src/App1.js
+++ src/App1.js
@@ -1,34 +1,60 @@
 import React from 'react';
 import './App1.css';
 
-function App () {
-  return (
-    <div>
-      <div className="header">
-        <Time1 />
-        <Judge />
-        <Time2 />
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      result1: 0,
+      result2: 0,
+    }
+    this.t0 = new Date()
+  }
+  success1 () {
+    console.log('兔子done,cost')
+    let r1 = new Date() - this.t0
+    this.setState({
+      result1: r1
+    })
+  }
+  success2 () {
+    console.log('乌龟done,cost')
+    let r2 = new Date() - this.t0
+    this.setState({
+      result2: r2
+    })
+  }
+
+
+  render () {
+    return (
+      <div>
+        <div className="header">
+          <Time1 result={this.state.result1} />
+          <Judge />
+          <Time2 result={this.state.result2} />
+        </div>
+        <Track1 success={this.success1.bind(this)} />
+        <Track2 success={this.success2.bind(this)} />
       </div>
-      <Track1 />
-      <Track2 />
-    </div>
-  )
+    )
+  }
 }
 
-function Time1 () {
+function Time1 (props) {
   return (
     <div>
       <h2>兔子用时</h2>
-      <div>0</div>
+      <div>{props.result}</div>
     </div>
   )
 }
 
-function Time2 () {
+function Time2 (props) {
   return (
     <div>
       <h2>乌龟用时</h2>
-      <div>0</div>
+      <div>{props.result}</div>
     </div>
   )
 }
@@ -42,13 +68,17 @@ class Track1 extends React.Component {
         transform: `translateX(${n}%)`
       }
     }
-    setInterval(() => {
-      n += 10
+    let timerId = setInterval(() => {
+      n += 20
       this.setState({
         style: {
           transform: `translateX(${n}%)`
         }
       })
+      if (n >= 100) {
+        window.clearInterval(timerId)
+        this.props.success()
+      }
     }, 1000)
   }
   render () {
@@ -70,13 +100,17 @@ class Track2 extends React.Component {
         transform: `translateX(${n}%)`
       }
     }
-    setInterval(() => {
-      n += 1
+    let timerId = setInterval(() => {
+      n += 10
       this.setState({
         style: {
           transform: `translateX(${n}%)`
         }
       })
+      if (n >= 100) {
+        window.clearInterval(timerId)
+        this.props.success()
+      }
     }, 1000)
   }
   render () {
