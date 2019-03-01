0a337749aff52dd258bbf6f98ffe08fafc18fe74
yangdepu <yangdepu@zdsoft.cn>
2019-03-01 14:32:32 +0800

    react-router

--- src/App.js
+++ src/App.js
@@ -1,33 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
-import Counter from './counter/counter';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Index from './component/index.jsx'
+import About from './component/about.jsx'
+import Welcome from './component/welcome.jsx'
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-      btnType: '?'
     }
   }
-  selectType(type){
-    if(type === 1){
-      this.setState({
-        btnType: '加'
-      })
-    }else{
-      this.setState({
-        btnType: '减'
-      })
-    }
-  }
+
   // 不建议在render函数中做this转换
   // 建议在constructor中做，this.handleClick = this.handleClick.bind(this)
   render() {
     return (
-      <div className="parent">
-        <p className="click-type">点了子组件的<span className="type">{this.state.btnType}</span>号</p>
-        <Counter name="App" selectType={this.selectType.bind(this)}/>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul className="nav">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Index}></Route>
+          <Route path="/about/" component={About}></Route>
+          <Route path="/users/" component={Welcome}></Route>
+        </div>
+      </Router>
     );
   }
 }
